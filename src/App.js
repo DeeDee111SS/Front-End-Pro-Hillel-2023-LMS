822b7dd02405bb698bcbe8e4701230aaad8cb4c6
Dmitrij Duvanov <dmitrijduvanov7@gmail.com>
2023-10-21 00:42:26 +0300

    basic file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 function App() {
   
   return (
     <div className="App">
-        
+      <BrowserRouter>
+        <Provider store={store}>
+          <TodoBox/>
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }
