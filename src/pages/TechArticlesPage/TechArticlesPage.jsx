40bf46ed6a5aa79179823e01acf38b626e737ed4
Dmitrij Duvanov <dmitrijduvanov7@gmail.com>
2023-10-25 01:10:06 +0300

    some styles added

--- src/pages/TechArticlesPage/TechArticlesPage.jsx
+++ src/pages/TechArticlesPage/TechArticlesPage.jsx
@@ -1,6 +1,11 @@
 import { useGetPostsQuery } from "../../store/articlesApi";
 import { useState } from "react";
 import Grid from '@mui/material/Grid';
+import { Paper } from "@mui/material";
+import Title from "../../components/Title";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function TechArticlesPage() {
     const [numberOfPosts, setNumberOfPosts] = useState('');
@@ -10,21 +15,46 @@ function TechArticlesPage() {
 
     const changeNumberOfPosts = ({target}) => setNumberOfPosts(target.value);
 
-    return <Grid container spacing={3}>
-                <div>TechArticlesPage</div>
-                <div className="Articles">
-                    <label>
-                        <span>Number for posts to show: |{numberOfPosts || 'all' }| :</span>
-                        <input type="number" onInput={changeNumberOfPosts} value={numberOfPosts}/>
-                    </label>
-                    <ul>
-                        {data.map(post => (
-                            <li key={post.id}>{post.title}</li>
-                        ))}
-                    </ul>
-                </div>
-            </Grid>
-    
+    return  <Grid container spacing={3}>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: 120,
+                        }}
+                    >
+                        <Title>Tech Articles Page</Title>
+                        <div className="Articles">
+                            <label>
+                                <span>Number for articles to show: |{numberOfPosts || 'all' }| :</span>
+                                <input type="number" onInput={changeNumberOfPosts} value={numberOfPosts}/>
+                            </label>
+                        </div>
+                    </Paper>
+                </Grid>   
+                {data.map(post => (
+                    <Grid item xs={12} md={4} lg={3} key={post.id}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 380,
+                            }}
+                        >
+                            <Title>{post.title}</Title>
+                            <Typography color="text.secondary" sx={{ flex: 1 }}>
+                                {post.body}
+                            </Typography>
+                            <Button variant="contained" endIcon={<ArrowForwardIosIcon />}>
+                                Read
+                            </Button>
+                        </Paper>
+                    </Grid>
+                ))} 
+            </Grid>    
 }
 
 export default TechArticlesPage;
